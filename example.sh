078c9b7e22e9e89c1353338094684b84f80bb243
zanzibar7 <tcr-public2@psu.edu>
2016-03-24 14:09:25 -0400

    spelling

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
-# fast mutation leads to maximation of duration and transmission
+# fast mutation leads to maximization of duration AND transmission
 ./spatialEpiEvo -N20 -W -x127 -y59 -b.01 -g40 -t100000 -m0.05
 
 # slow mutation leads to a trade-off between transmission and recovery
